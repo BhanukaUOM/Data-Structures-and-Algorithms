4dd709ffe2ccdb535043a9536673d2d52e4ff679
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-24 13:51:31 +0530

    Create 3. Queue.java

---
+++ 1. Java DS/3. Queue.java
@@ -0,0 +1,25 @@
+import java.util.ArrayDeque; // better than Linkedlist 
+
+ 
+Queue<Node> queue = new LinkedList<Node>(); 
+Push = q.add(1); //add first
+Remove = q.remove(); //remove and get first
+Pop = q.pop(); 
+peek() // get head, not delete 
+poll() = remove and get head(First). 
+insert()
+ 
+ 
+ 
+java.util.LinkedList; 
+ 
+ArrayList<Integer> list = new ArrayList<>();   
+List.remove();…. 
+ 
+Or
+
+import java.util.LinkedList; 
+ 
+add, (Queue) 
+isEmpty, 
+poll (DeQueue) 
