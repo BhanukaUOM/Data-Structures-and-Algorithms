2c15645c1f0ec6a18fa7dcb272c88b3fb17ca2a8
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-24 14:00:17 +0530

    Create python.py

---
+++ 2. Statistics/python.py
@@ -0,0 +1,24 @@
+import statistics 
+ 
+#9.7.1. Averages and measures of central location
+
+  #These functions calculate an average or typical value from a population or sample. 
+
+    mean() #Arithmetic mean (“average”) of data. 
+    harmonic_mean() #Harmonic mean of data. 
+    median() #Median (middle value) of data. 
+    median_low() #Low median of data. 
+    median_high() #High median of data. 
+    median_grouped() #Median, or 50th percentile, of grouped data. 
+    mode() #Mode (most common value) of discrete data. 
+
+
+#9.7.2. Measures of spread 
+
+  #These functions calculate a measure of how much the population or sample tends to deviate from the typical or average values. 
+  
+    pstdev() #Population standard deviation of data. 
+    pvariance() #Population variance of data. 
+    stdev() #Sample standard deviation of data. 
+    variance() #Sample variance of data. 
+ 
