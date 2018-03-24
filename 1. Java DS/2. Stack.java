94030461ea087cc9c665708a79d73ebbfd5e6a15
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-24 13:47:56 +0530

    Create 2. Stack.java

---
+++ 1. Java DS/2. Stack.java
@@ -0,0 +1,15 @@
+import java.util.Stack; 
+ 
+Stack<Integer> stack = new Stack<Integer>(); 
+         
+stack.add(index(opt), 1); //insert
+empty(); 
+peak(); //get last without remove
+pop(); //remove and get last
+push(elem); //add last
+stack.search(args); 
+ 
+ 
+java.util.LinkedList; 
+ 
+List.pop();…. 
