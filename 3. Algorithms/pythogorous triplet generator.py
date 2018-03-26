6782be8d2bc097d872202eaedc45cf0d91a3a6a0
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-27 01:01:19 +0530

    Create pythogorous triplet generator.py

---
+++ 3. Algorithms/pythogorous triplet generator.py
@@ -0,0 +1,11 @@
+def pythagoreanTriplets(limits) :
+    c, m = 0, 2
+    while c < limits :
+        for n in range(1, m) :
+            a = m * m - n * n
+            b = 2 * m * n
+            c = m * m + n * n
+            if c > limits :
+                break
+            print(a, b, c) 
+        m = m + 1
