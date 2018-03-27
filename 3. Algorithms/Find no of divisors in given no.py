d74ef66e57b654f4a451c9df7cd72ae877020053
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-27 19:12:56 +0530

    Update Find no of divisors in given no.py

--- 3. Algorithms/Find no of divisors in given no.py
+++ 3. Algorithms/Find no of divisors in given no.py
@@ -8,10 +8,10 @@ for i in range(2,200000):
         continue 
     for j in range(2*i,200000,i): 
         a[j] = 1 
-primes = [] 
+primes = {}
 for i in range(2,200000): 
     if a[i] == 0: 
-        primes.append(i) 
+        primes[i] = True 
 ans = 1
 for p in primes :
     if p*p*p > N:
