e83ac40416ebd9533a000d46f629ceb390fe1892
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-29 16:34:02 +0530

    Create 6. Connectivity Array.java

---
+++ 6. Graph Theory/6. Connectivity Array.java
@@ -0,0 +1,23 @@
+static boolean result;
+    static boolean is_connect(boolean[][] arr,int n, int u, int w){
+        result = false;
+        boolean[] visited = new boolean[n];
+        visited[u] = true;
+        DFS(arr,n, u, w, visited);
+        return result;
+    }
+
+    static void DFS(boolean[][] arr,int n, int u, int w,boolean[] visited){
+        if (result==false){
+            if(u==w)
+                result=true;
+            else {
+                for(int i=0; i<n; i++){
+                    if(arr[u][i] && !visited[i]){
+                        visited[i] = true;
+                        DFS(arr,n, i, w, visited);
+                    }
+                }
+            }
+        }
+    }
