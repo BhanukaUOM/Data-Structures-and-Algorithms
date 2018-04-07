38e0e0a9b13bdfd07f147667ef102da5160ca232
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-04-07 22:44:41 +0530

    Create 05. sum of all divisors.py

---
+++ 3. Algorithms/Maths/05. sum of all divisors.py
@@ -0,0 +1,13 @@
+n = 100001
+
+div_sum = [0 for i in range(n)]
+
+for i in range(2, n):
+    sq = int(math.sqrt(i))
+    for j in range(1, sq+1):        
+        if i%j==0:
+            p = i//j
+            if p!=i:
+                div_sum[i] += j + p
+            else:
+div_sum[i] += j
