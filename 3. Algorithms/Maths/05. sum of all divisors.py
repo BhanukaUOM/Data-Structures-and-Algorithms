b5e72798db43fa31ad60689072e787804555eaba
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-04-07 23:25:06 +0530

    Update 05. sum of all divisors.py

--- 3. Algorithms/Maths/05. sum of all divisors.py
+++ 3. Algorithms/Maths/05. sum of all divisors.py
@@ -9,7 +9,7 @@ for i in range(2, n):
     for j in range(1, sq+1):        
         if i%j==0:
             p = i//j
-            if p!=i:
+            if p!=j:
                 div_sum[i] += j + p
             else:
                 div_sum[i] += j
