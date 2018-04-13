a27b85fd187d68110f77194f4a4dba9748ba28d4
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-04-14 01:06:13 +0530

    Top-Down & Bottom-up

--- 5. Trees/3. Tree Height.java
+++ 5. Trees/3. Tree Height.java
@@ -1,12 +1,40 @@
-static int height(Node root) { 
-        if(root != null){ 
-            int l = height(root.left)+1; 
-            int r = height(root.right)+1; 
-             
-            if(l<r) 
-                return r; 
-            else 
-                return l; 
-        } else 
-            return -1; 
-    } 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+//TOP-DOWN
+class Solution {
+    int max = 0;
+    
+    public int maxDepth(TreeNode root) {
+        depth(root, 0);
+        return max;
+    }
+    
+    public void depth(TreeNode node, int count){
+        if(node == null){
+            if(count>max)
+                max = count;
+        } else {
+            depth(node.left, count+1);
+            depth(node.right, count+1);
+        }
+    }
+}
+
+//BOTTOM-UP
+import java.lang.*;
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
+    }
+}
