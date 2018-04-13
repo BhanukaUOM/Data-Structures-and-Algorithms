ae111d75cc4800d42cf0f525ad82854bbc48ce39
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-04-14 01:52:46 +0530

    Create 1. Binary Search.java

---
+++ 1. Java DS/1. Binary Search.java
@@ -0,0 +1,15 @@
+int binarySearch(int[] nums, int target){
+  if(nums == null || nums.length == 0)
+    return -1;
+
+  int left = 0, right = nums.length - 1;
+  while(left <= right){
+    int mid = left + (right - left) / 2;
+    if(nums[mid] == target){ return mid; }
+    else if(nums[mid] < target) { left = mid + 1; }
+    else { right = mid - 1; }
+  }
+  return -1;
+}
+
+
