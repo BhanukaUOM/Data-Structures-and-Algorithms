7b2abebe8942ce01847c5cb61877e7bfd95bd28e
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-10-16 19:27:20 +0530

    Update 01. Find All Prime numbers.java

--- 3. Algorithms/Maths/01. Find All Prime numbers.java
+++ 3. Algorithms/Maths/01. Find All Prime numbers.java
@@ -1,3 +1,12 @@
+def SieveOfEratosthenes(n): 
+    prime = [True for i in range(n+1)] 
+    p = 2
+    while (p*p <= n): 
+        if (prime[p] == True): 
+            for i in range(p*2, n+1, p): 
+                prime[i] = False
+        p += 1
+                    
 /* Python
 IsPrime(O(sqrt(N))) 
 def isprime(n): 
